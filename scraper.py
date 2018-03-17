b3702edf8628e8f16a2ca3216d07af5500b7d833
lastbulletbender <abhra.chatto@gmail.com>
2018-03-17 12:29:20 +0530

    Create queue of browsers

--- scraper.py
+++ scraper.py
@@ -1,55 +1,13 @@
+import seleniumqueue
 from lxml import html
 import time
-import selenium
-import os
-from selenium.common.exceptions import TimeoutException
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 import datafetcher
 from diskcache import Cache
 
 #Required for running selenium on server
-from pyvirtualdisplay import Display
-
-display = Display(visible=0, size=(800, 600))
-display.start()
 
 attendance_cache = Cache('/tmp/mycachedir/attendance')
 marks_cache = Cache('/tmp/mycachedir/marks')
-
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-timeout = 3
-
-def go_to_homepage(registration_number,password):
-	browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",chrome_options=options)
-  	browser.get("http://evarsity.srmuniv.ac.in/srmswi/usermanager/ParentLogin.jsp")
-	try:
-		assert "SRM" in browser.title
-	except:
-		browser.quit()
-		raise TimeoutException
-	try:
-		element_present = EC.presence_of_element_located((By.ID, 'txtRegNumber'))
-		WebDriverWait(browser, timeout).until(element_present)
-        except TimeoutException:
-		browser.quit()
-		raise TimeoutException
-
-        registration_number_element = browser.find_element_by_name("txtRegNumber")
-        password_element = browser.find_element_by_id("txtPwd")
-        images = browser.find_elements_by_tag_name('img')
-        registration_number_element.send_keys(registration_number)
-        password_element.send_keys(password)
-        time.sleep(1)
-        images[0].click()
-	return browser
 
 def scrape(registration_number,password,action,args=False):
 	try:
@@ -67,9 +25,7 @@ def scrape(registration_number,password,action,args=False):
 
 def get_attendance(registration_number,password,args):
 	if (attendance_cache.get(registration_number) is None):
-		browser = go_to_homepage(registration_number,password)
-		browser.get("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=7")
-		html = browser.page_source
+		html = seleniumqueue.get_source("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=7")
 		attendance_cache.set(registration_number,html,expire=30)
 		browser.quit()
 	else:
@@ -82,10 +38,7 @@ def get_attendance(registration_number,password,args):
 
 def get_marks(registration_number,password,args):
 	if (marks_cache.get(registration_number) is None):
-		browser = go_to_homepage(registration_number,password)
-		browser.get("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=16")
-		html = browser.page_source
-		browser.quit()
+		html = seleniumqueue.get_source("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=16")
 		message = datafetcher.scrape_marks_all(html)
 		marks_cache.set(registration_number,message,expire=120)
 	else:
@@ -93,20 +46,13 @@ def get_marks(registration_number,password,args):
 	return message
 
 def get_timetable(registration_number,password,args):
-	browser = go_to_homepage(registration_number,password)
-	browser.get("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=5")
-	html = browser.page_source
-	browser.quit()
+	html = seleniumqueue.get_source(registration_number,password,"http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=5")
 	message = datafetcher.scrape_timetable_all(html)
 	return message
 
 def check_registration(registration_number,password):
-	browser = go_to_homepage(registration_number,password)
-	browser.get("http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=1")
-	html = browser.page_source
+	html = seleniumqueue.get_source(registration_number,password,"http://evarsity.srmuniv.ac.in/srmswi/resource/StudentDetailsResources.jsp?resourceid=1")
 	if "Father Name" not in html:
-		browser.quit()
 		raise AssertionError()
 	name = datafetcher.validate_registration(html)
-	browser.quit()
 	return name
---
+++ seleniumqueue.py
@@ -0,0 +1,57 @@
+import Queue
+import time
+import selenium
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+
+#Required to running selenium on server
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+
+q = Queue.Queue(4)
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+timeout = 3
+
+def get_source(reg_num,pwd,url):
+	browser = q.get()
+
+	try:
+	        element_present = EC.presence_of_element_located((By.ID, 'txtRegNumber'))
+	        WebDriverWait(browser, timeout).until(element_present)
+        except TimeoutException:
+	        print "Timed out waiting for page to load"
+		add_to_queue(browser)
+		raise TimeoutException
+	registration_number_element = browser.find_element_by_name("txtRegNumber")
+	password_element = browser.find_element_by_id("txtPwd")
+	registration_number_element.send_keys(reg_num)
+        password_element.send_keys(pwd)
+	images = browser.find_elements_by_tag_name('img')
+	time.sleep(1)
+        images[0].click()
+	browser.get(url)
+	source = browser.page_source 
+	add_to_queue(browser)
+	return source
+
+def add_to_queue(browser):
+	browser.delete_all_cookies()
+	browser.get("http://evarsity.srmuniv.ac.in/srmswi/usermanager/ParentLogin.jsp")
+	q.put(browser)
+
+
+print q.qsize()
+for i in range(0,4):
+	browser = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver",chrome_options=options)
+	add_to_queue(browser)
